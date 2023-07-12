5cd24f951b684604fb11ac65212b598bf6ee8787
REI209 <138412628+REI209@users.noreply.github.com>
2023-07-12 12:11:23 +0900

    追加1

--- gameprograming/Match3/GameMainScene_.cpp
+++ gameprograming/Match3/GameMainScene_.cpp
@@ -34,3 +34,110 @@ intNUmberIMage[NUMBER_IMAGE_MAX];   //数字用画像
 * 引数：なし
 * 戻り値：エラー情報
 *************************************/
+int GameMainScene_Intialize(void)
+{
+	int ret = 0;
+
+	int i;
+
+
+   //画像読み込み
+   LoadDivGraph("images/number.png", NUMBER_IMAGE_MAX,
+	NUMBER_IMAGE_MAX, 1, 60, 120, NumberImage);
+
+    //ステージ機能初期化
+    ret = StageInitialize();
+
+  //エラーチェック
+  for (i = 0; i < NUMBER_IMAGE_MAX; i++)
+  {
+	if (NumberImage[i] == D_ERROR)
+	{
+		ret = D_ERROR;
+		break;
+	}
+  }
+  //ゲームプレイが初回かどうか
+  if (GameCount == 0)
+  {
+	GameScore = 0;         //スコアの初期化
+	GameLevel = 1;         //ゲームレベルの初期化
+	SetStageMIssion(3);    //ミッションの初期化
+	GameCount++;           //次回の設定
+  }
+  else
+  {
+	GameLevel++;         //ゲームレベルの更新
+	Set_StageMission(3); //ミッションを増やす
+  }
+   GameTime = TIMELIMIT;
+
+   return ret;
+}
+/***********************************
+*ゲームメイン画面：更新処理
+* 引数：なし
+* 戻り値：なし
+************************************/
+void GameMainScene_Update(void)
+{
+	switch (GetStageState())
+	{
+		case 0:
+		    SelectBlock();
+		    break;
+   
+		case 2:
+			MoveBlock();
+			break;
+
+		case 3
+			CheckBlock();
+			break;
+
+		case 4:
+			CheakClear();
+			break;
+	
+		default:
+			break;
+	}
+
+	//制限時間の更新
+	GameTime--;
+
+	//制限時間が無くなったら、ゲームオーバーに簡易する
+	if (GameTime < 0)
+	{
+		Cange_Scene(E_GAME_OVER);
+	}
+	//ミッションを達成したら、ゲームクリア
+	if (Get_StageClearFlag())
+	{
+		Change_Scene(E_GAME_CLEAR);
+	}
+}
+/**************************************
+*ゲームメイン画面：描画処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void GameMainScene_Draw(void)
+{
+	int PosX = 600;
+	int tmp_level = GameLevel;
+	int tmp_score = Get_StageScore();
+
+	//ステージを描画
+	StageDraw();
+
+	//フェードアウト状態か
+	if (Get_StageState() == 1)
+	{
+		FadeOutBlock();   //フェードアウトする
+	}
+	//レベルを描画
+	do {
+		DrawRotaGraph(PosX, 80, 0.5f, 0, NUmberImage[tmp_level % 10], TRUE);
+	}
+ }
